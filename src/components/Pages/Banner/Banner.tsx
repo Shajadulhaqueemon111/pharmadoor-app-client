2aa9c6df2adafd70a90742704ca6b3cb2fd0cc88
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-05-14 17:37:20 +0600

    comlete this banner section

--- src/components/Pages/Banner/Banner.tsx
+++ src/components/Pages/Banner/Banner.tsx
@@ -4,33 +4,44 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Style.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-
-import logo1 from "../../../assets/Image.png";
+import medicalImage from "../../../assets/medical-deliver-app.webp";
+// import logo1 from "../../../assets/Image.png";
+import logo2 from "../../../assets/medicine-delivery.jpg";
+import logo3 from "../../../assets/maxresdefault.jpg";
 const BannerPage = () => {
+  const imazeStyle = {
+    height: "450px",
+    witdth: "400px",
+  };
   return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src={logo1} alt="logo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={logo1} alt="logo" />
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <div>
+      <>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img style={imazeStyle} src={medicalImage} alt="logo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img style={imazeStyle} src={logo2} alt="logo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img style={imazeStyle} src={logo3} alt="logo" />
+          </SwiperSlide>
+        </Swiper>
+      </>
+    </div>
   );
 };
 
