7af5a5f10fb3f9909d488f3d3f5f990c066092fe
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-08-04 18:05:26 +0600

    successfully completed project

--- src/components/Pages/Firebase/Firebase.ts
+++ src/components/Pages/Firebase/Firebase.ts
@@ -2,14 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCQ4YY1GON8DAJbQqZbU8AlHphoLp9d5Rw",
-  authDomain: "authentication-pharmadoor.firebaseapp.com",
-  projectId: "authentication-pharmadoor",
-  storageBucket: "authentication-pharmadoor.firebasestorage.app",
-  messagingSenderId: "525166565286",
-  appId: "1:525166565286:web:21b866ac3169c946de6167",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 
